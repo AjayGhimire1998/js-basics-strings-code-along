a67ec3931bcdabddabdbae807353bf0fe686bf7e
AjayGhimire <ajayghimire1998@gmail.com>
2022-02-26 21:03:25 +1100

    commit tree 9ba09ed1aa20d1509771de50bc680ff92983995b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this fill
+const currentUser = "Ajay";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
